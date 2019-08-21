36d8562a283484c831fb0931e0d78f38a2b5b796
gitdema <khersonec.12@gmail.com>
2019-08-21 03:57:58 +0300

    getting weather data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import axios from "axios";
+import { CountryDropdown, RegionDropdown } from "react-country-region-selector";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import WeatherList from "./WeatherList";
+import "./css/App.css";
+
+const weather_api_key = "b81db6432bb2b2d4ab273de0852795d6";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { country: "", region: "", weatherByCity: null };
+  }
+
+  selectCountry(val) {
+    this.setState({ country: val });
+  }
+
+  selectRegion(val) {
+    this.setState({ region: val }, this.getWeatherData);
+  }
+
+  getWeatherData = () => {
+    const { region } = this.state;
+
+    axios({
+      method: "get",
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${region}&appid=${weather_api_key}&units=metric`
+    })
+      .then(res => this.setState({ weatherByCity: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    const { country, region, weatherByCity } = this.state;
+
+    return (
+      <div className="app">
+        <div className="app-main">
+          <div className="selection">
+            <p>Country:</p>
+            <CountryDropdown
+              className="select"
+              value={country}
+              onChange={val => this.selectCountry(val)}
+            />
+          </div>
+
+          {country ? (
+            <div className="selection">
+              <p>City:</p>
+              <RegionDropdown
+                className="select"
+                country={country}
+                value={region}
+                onChange={val => this.selectRegion(val)}
+              />
+            </div>
+          ) : null}
+
+          {weatherByCity ? (
+            <Fragment>
+              <WeatherList data={weatherByCity} />
+              <button className="btn">Save</button>
+            </Fragment>
+          ) : null}
+        </div>
+
+        <div>Weather</div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/WeatherList.js
@@ -0,0 +1,55 @@
+import React from "react";
+import moment from "moment";
+
+import "./css/WeatherData.css";
+
+const dataList = ({ data }) => {
+  return (
+    <div className="weather-data">
+      <div className="weather-data-main">
+        <img
+          src={`http://openweathermap.org/img/wn/${
+            data.weather[0].icon
+          }@2x.png`}
+          alt="icon"
+        />
+        <p>{data.weather[0].description}</p>
+      </div>
+
+      <ul className="weather-data-list">
+        <li className="weather-data-list_item">
+          <p className="weather-data-list_item-title">
+            Temperature <span>(&#8451;)</span>:
+          </p>
+          <p className="weather-data-list_item-value">
+            {Math.round(data.main.temp)}
+          </p>
+        </li>
+        <li className="weather-data-list_item">
+          <p className="weather-data-list_item-title">
+            Wind <span>(meter/sec)</span>:
+          </p>
+          <p className="weather-data-list_item-value">
+            {Math.round(data.wind.speed)}
+          </p>
+        </li>
+        <li className="weather-data-list_item">
+          <p className="weather-data-list_item-title">
+            Humidity <span>(%)</span>:
+          </p>
+          <p className="weather-data-list_item-value">{data.main.humidity}</p>
+        </li>
+        <li className="weather-data-list_item">
+          <p className="weather-data-list_item-title">
+            Coordinates <span>(long/lat)</span>:
+          </p>
+          <p className="weather-data-list_item-value">
+            {data.coord.lon} / {data.coord.lat}
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default dataList;
